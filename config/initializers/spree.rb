3b8b52459fb9b32a89ae1241b3249a94ebf4fd0d
Samuel <sassani134@gmail.com>
2023-08-17 13:45:00 +0200

    EUR currency

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -7,7 +7,7 @@ Spree.load_defaults '4.1.1'
 Spree.config do |config|
   # Core:
   # Default currency for new sites
-  config.currency = "USD"
+  config.currency = "EUR"
 
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
